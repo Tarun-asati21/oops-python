19b91f6722ea4f4fe2db8052697831bf69d5515c
Tarun-asati21 <tarunasati96@gmail.com>
2026-09-11 17:58:42 +0530

    8 : function vs method difference

--- rough.py
+++ rough.py
@@ -4,4 +4,16 @@
 from oops_demo_project import chatbook
 
 # creating object 
+# user1 = chatbook()
+
+# difference between function and method
+
+lst=[1,2,3]
+
+# function --> isme hume data structure/parameters ko pass karte hai manually
+a1 = len(lst)
+print(a1)
+
+# method --> isme hum object ko pakadkar, dot lagakr method call karte hai directly
 user1 = chatbook()
+user1.sendmsg()
