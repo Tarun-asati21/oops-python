01f0f2a6087a0651928980894f695845a7e7f723
Tarun-asati21 <tarunasati96@gmail.com>
2026-09-11 17:40:56 +0530

    7 : learn how to use module in other files by importing them

---
+++ rough.py
@@ -0,0 +1,7 @@
+# how to import a particular class from different file, and use it
+# syntax : from {file_name} import {class_name}
+
+from oops_demo_project import chatbook
+
+# creating object 
+user1 = chatbook()
