694f0ad271593b8d321b7b5c9c23f93034e37d33
Tarun-asati21 <tarunasati96@gmail.com>
2026-09-11 17:16:58 +0530

    5 : messaging feature added

--- oops_demo_project.py
+++ oops_demo_project.py
@@ -6,15 +6,13 @@ class chatbook :
         self.menu() # method ko constructor ke aandar hi call kar skte hai
         
     def menu(self):
-        user_input = input("""
-                           Welcome to CHATBOOK !! How would you like to proceed?
-                           
+        user_input = input("""Welcome to CHATBOOK !! How would you like to proceed?
                            1. Press 1 to Signup
                            2. Press 2 to Signin
                            3. Press 3 to write a post
                            4. Press 4 to message a friend
                            5. Press any other key to exit
-                           """)
+                           -->""")
         
         if user_input == "1" :
             self.signup()
@@ -23,9 +21,9 @@ class chatbook :
         elif user_input == "3" :
             self.my_post()
         elif user_input == "4" :
-            pass
+            self.sendmsg()
         else :
-            exit()
+            exit() # inbuit keyword in python to end a code
             
     def signup(self) :
         email = input("enter your email here -> ")
@@ -63,5 +61,15 @@ class chatbook :
         print("\n")
         self.menu()
             
+    def sendmsg(self) :
+        if self.loggedin==True :
+            txt = input("enter your message here -> ")
+            frnd = input("whom to send the msg ?")
+            print(f"your message has been sent to {frnd}")
+        else :
+            print("you need to signin first to post something...")
+            
+        print("\n")
+        self.menu()
         
 obj = chatbook()
