b6791f37c328a49af0f00eb52783ec9f1cb1bbd8
Tarun-asati21 <tarunasati96@gmail.com>
2026-09-11 16:48:27 +0530

    2 : demo project basic landing page ready

---
+++ oops_demo_project.py
@@ -0,0 +1,31 @@
+class chatbook :
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        self.loggedin = False
+        self.menu() # method ko constructor ke aandar hi call kar skte hai
+        
+    def menu(self):
+        user_input = input("""
+                           Welcome to CHATBOOK !! How would you like to proceed?
+                           
+                           1. Press 1 to Signup
+                           2. Press 2 to Signin
+                           3. Press 3 to write a post
+                           4. Press 4 to message a friend
+                           5. Press any other key to exit
+                           """)
+        
+        if user_input == "1" :
+            pass
+        elif user_input == "2" :
+            pass
+        elif user_input == "3" :
+            pass
+        elif user_input == "4" :
+            pass
+        else :
+            exit()
+            
+            
+obj = chatbook()
